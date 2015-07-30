ece0ee2ea0620f89113cf14e1e387e28b3dc1d29
Brett Lyman <github@brettlyman.com>
2015-07-29 21:06:09 -0600

    Now plotting sma on the chart, starts out zoomed in

--- blog/js/transformDiv.js
+++ blog/js/transformDiv.js
@@ -9,21 +9,12 @@ define([
 ], function (d3, sl, MockData, /*tickWidth,*/ moment) {
     'use strict';
 
-    // var mockData = new MockData(0, 0.1, 100, 50, function (moment) {
-        // return !(moment.day() === 0 || moment.day() === 6);
-    // });
-
-    // var fromDate = new Date(2012, 8, 1),
-        // toDate = new Date(2014, 8, 1);
-
-    // var data = mockData.generateOHLC(fromDate, toDate);
-	
 	var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    windowWidth = w.innerWidth || e.clientWidth || g.clientWidth,
-    windowHeight = w.innerHeight|| e.clientHeight|| g.clientHeight;
+        d = document,
+        e = d.documentElement,
+        g = d.getElementsByTagName('body')[0],
+        windowWidth = w.innerWidth || e.clientWidth || g.clientWidth,
+        windowHeight = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
     var margin = {top: 20, right: 20, bottom: 30, left: 50},
         width = windowWidth - margin.left - margin.right - 25,
@@ -43,24 +34,27 @@ define([
         .scale(yScale)
         .orient('left');
 
-    var series = sl.series.candlestick()
+    var candles = sl.series.candlestick()
         .xScale(xScale)
-        .yScale(yScale)
-        .rectangleWidth(5); // initial value until data loads or zoom happens
+        .yScale(yScale);
+
+    var sma = d3.svg.line()
+        .x(function(d) { return xScale(d.date); })
+        .y(function(d) { return yScale(d.sma); });
 
     var zoom = d3.behavior.zoom()
         .x(xScale)
         .scaleExtent([0.5, 500])
         .on('zoom', zoomed)
         .on('zoomend', zoomend);
-		
+
 	var parseDate = d3.time.format("%Y.%m.%d %H:%M:%S").parse;
-	
+
 	var symbol = d3.select("#symbol")
 			.on("change", load);
 	var granularity = d3.select("#granularity")
 			.on("change", load);
-			
+
 	function granularityToSeconds() {
 		switch (granularity.node().value) {
 			case "1m":
@@ -75,7 +69,7 @@ define([
 				return 86400;
 		}
 	}
-	
+
 	function rectangleWidth() {
         var scaleRange = xScale.range();
 		var domain = xScale.domain();
@@ -89,31 +83,95 @@ define([
 			if (error) {
 				return alert(error);
 			}
-			
-			var accessor = function (d) { return d.date; };
 
+            d3.select('#chart').selectAll("svg").remove();
+            d3.select("#series-clip").remove();
+
+            var clipDiv = d3.select('#chart').classed('chart', true).append('div')
+                .attr('id', 'series-clip')
+                .style('position', 'absolute')
+                .style('overflow', 'hidden')
+                .style('top', margin.top + 'px')
+                .style('left', margin.left + 'px');
+
+            var seriesDiv = clipDiv.append('div')
+                .attr('id', 'series-container');
+
+            var svg = d3.select('#chart').append('svg')
+                .style('position', 'absolute')
+                .attr('width', width + margin.left + margin.right)
+                .attr('height', height + margin.top + margin.bottom);
+        		/*
+        	   //responsive SVG needs these 2 attributes and no width and height attr
+        	   .attr("preserveAspectRatio", "xMinYMin meet")
+        	   .attr("viewBox", "0 0 1000 575")
+        	   //class to make it responsive
+        	   .classed("svg-content-responsive", true);
+        	   */
+
+            var seriesSvg = seriesDiv.append('svg')
+                .attr('width', width)
+                .attr('height', height);
+
+            // Ceate chart
+            var g = svg.append('g')
+                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+            // Create plot area
+            var plotArea = g.append('g');
+            plotArea.append('clipPath')
+                .attr('id', 'plotAreaClip')
+                .append('rect')
+                .attr({ width: width, height: height });
+            plotArea.attr('clip-path', 'url(#plotAreaClip)');
+
+            plotArea.append('rect')
+                .attr('class', 'zoom-pane')
+                .attr('width', width)
+                .attr('height', height)
+                .call(zoom);
+
+            var smaValues = [];
 			data = jsonData.slice(0,5000).map(function(d) {
+                if (smaValues.length == 5)
+                    smaValues.shift();
+                smaValues.push(d.close);
+                var sum = 0;
+                for (var i = 0; i < smaValues.length; i++)
+                    sum += smaValues[i];
 				return {
 					date: parseDate(d.timestamp),
 					open: +d.open,
-					high: +d.high,	
+					high: +d.high,
 					low: +d.low,
 					close: +d.close,
-					volume: +d.volume
+					volume: +d.volume,
+                    sma: +(sum / smaValues.length)
 				};
-			});//.sort(function(a, b) { return d3.ascending(accessor.d(a), accessor.d(b)); });
-			
+			});
+
 			// Set scale domains
-			xScale.domain(d3.extent(data, function (d) {
+            xScale.range(
+                [
+                    d3.min(data, function (d) {
+                        return d.low;
+                    }),
+                    d3.max(data, function (d) {
+                        return d.high;
+                    })
+                ]
+            );
+            var dataSlice = data.slice(data.length-150,data.length); // try to zoom to most recent 100 bars
+			xScale.domain(d3.extent(dataSlice, function (d) {
 				return d.date;
 			}));
 
 			yScale.domain(
 				[
-					d3.min(data, function (d) {
+					d3.min(dataSlice, function (d) {
 						return d.low;
 					}),
-					d3.max(data, function (d) {
+					d3.max(dataSlice, function (d) {
 						return d.high;
 					})
 				]
@@ -136,13 +194,18 @@ define([
 				.attr('class', 'y axis')
 				.call(yAxis);
 
-			// Draw series.
-			series.rectangleWidth(rectangleWidth());
+			// Draw candlesticks
+			candles.rectangleWidth(rectangleWidth());
 			seriesSvg.append('g')
 				.attr('class', 'series')
 				.datum(data)
-				.call(series);
-			
+				.call(candles);
+
+            // Draw sma
+            seriesSvg.append("path")
+                .attr("class", "sma")
+                .attr("d", sma(data));
+
 		});
 	}
 
@@ -211,63 +274,18 @@ define([
         oldScale = yScale.copy();
 
         zoom.x(xScale);
-        series.rectangleWidth(rectangleWidth());
+        candles.rectangleWidth(rectangleWidth());
 
         seriesDiv.select('.series')
-            .call(series);
+            .call(candles);
+
+        seriesDiv.select('.sma')
+            .attr('d', sma(data));
 
         seriesDiv = document.getElementById('series-container');
         seriesDiv.style.webkitTransform = nullTransform;
         seriesDiv.style.MozTransform = nullTransform;
-
     }
-
-    // Create svg element
-
-    var clipDiv = d3.select('#chart').classed('chart', true).append('div')
-        .attr('id', 'series-clip')
-        .style('position', 'absolute')
-        .style('overflow', 'hidden')
-        .style('top', margin.top + 'px')
-        .style('left', margin.left + 'px');
-
-
-    var seriesDiv = clipDiv.append('div')
-        .attr('id', 'series-container');
-
-    var svg = d3.select('#chart').append('svg')
-        .style('position', 'absolute')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom);
-		/*
-	   //responsive SVG needs these 2 attributes and no width and height attr
-	   .attr("preserveAspectRatio", "xMinYMin meet")
-	   .attr("viewBox", "0 0 1000 575")
-	   //class to make it responsive
-	   .classed("svg-content-responsive", true);
-	   */
-
-    var seriesSvg = seriesDiv.append('svg')
-        .attr('width', width)
-        .attr('height', height);
-
-    // Ceate chart
-    var g = svg.append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-    // Create plot area
-    var plotArea = g.append('g');
-    plotArea.append('clipPath')
-        .attr('id', 'plotAreaClip')
-        .append('rect')
-        .attr({ width: width, height: height });
-    plotArea.attr('clip-path', 'url(#plotAreaClip)');
-
-    plotArea.append('rect')
-        .attr('class', 'zoom-pane')
-        .attr('width', width)
-        .attr('height', height)
-        .call(zoom);
 
     load();
 });
